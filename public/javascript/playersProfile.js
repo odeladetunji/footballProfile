db5ad1daef23c9a916f77879ea73f12b090d76f2
unknown <odeladetunji@gmail.com>
2018-07-04 12:35:25 +0100

    committing

--- public/javascript/playersProfile.js
+++ public/javascript/playersProfile.js
@@ -1,6 +1,88 @@
 
 // landing section of the site!
 function playersProfile(props) {
+    function headerSection(){
+        return(<div className='headerSection'>
+                  <ul>
+                      <li>Home</li>
+                      <li>Players</li>
+                      <li>About</li>
+                      <li>Blog</li>
+                      <li>Contact</li>
+                  </ul>
+              </div>);
+    }
+
+    function profilePicture(){
+        var firstname = props.homePageData.playersProfile.firstName;
+        var lastname = props.homePageData.playersProfile.lastName;
+        return(<div className="playersProfilePictureSection">
+                    <ul>
+                        <li><img src = {'/' + props}></img></li>
+                        <li>{firstname + " " + lastname}</li>
+                    </ul>
+               </div>);
+    }
+
+    function mainBodySection(){
+        var height = props.homePageData.playersProfile.height;
+        var weight = props.homePageData.playersProfile.weight;
+        var birthday = props.homePageData.playersProfile.birthday;
+        var nationality = props.homePageData.playersProfile.nationality;
+        var ratingScore = props.homePageData.playersProfile.ratingScore;
+        return(
+            <div>
+                <ul>
+                    <li>{height}</li>
+                    <li>{weight}</li>
+                    <li>{birthday}</li>
+                    <li>{nationality}</li>
+                    <li>{ratingScore}</li>
+                </ul>
+                <div>
+                    <div>
+                        <h2>Player Details</h2>
+                        <ul>
+                            <li>Nickname: {}</li>
+                            <li>Address: {}</li>
+                            <li>PhoneNumber: {}</li>
+                            <li>Playing Foot: {}</li>
+                            <li>Playing Position: {}</li>
+                            <li>Club: {}</li>
+                        </ul>
+                        <h2>Playing Abilities</h2>
+                        
+                        <p>Speed</p>
+                        <div className="abilities"><div id="speed"></div></div>
+                        <p>Technique</p>
+                        <div className="abilities"><div id="technique"></div></div>
+                        <p>Dribblling</p>
+                        <div className="abilities"><div id="dribbling"></div></div>
+                        <p>Stamina</p>
+                        <div className="abilities"><div id="Stamina"></div></div>
+                        <p>Heading/Jump</p>
+                        <div className="abilities"><div id="heading"></div></div>
+                        <p>Pass</p>
+                        <div className="abilities"><div id="pass"></div></div>
+                        <p>Marking</p>
+                        <div className="abilities"><div id="marking"></div></div>
+                        <p>Free Kick</p>
+                        <div className="abilities"><div id="freeKick"></div></div>
+                        <p>Goal Keeping</p>
+                        <div className="abilities"><div id="goalKeeping"></div></div>
+                    </div>
+                    <div className="youtubeVideos">
+                         <ul>
+                             <li></li>
+                             <li></li>
+                             <li></li>
+                         </ul>
+                         <video></video>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     function commentSection(params) {
         var comments = prpps.comments.map(function(elements){
